895242fe83998465426d5cdf52d285a0f8231803
Om Budhiraja <ombudhiraja144@gmail.com>
2022-07-11 20:32:53 +0530

    retrying with same logic

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -3,13 +3,11 @@ import type { NextFetchEvent, NextRequest } from 'next/server';
 import { nanoid } from 'nanoid';
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-  console.log(req.cookies, req.cookies['poll-token']);
-
   if (req.cookies['poll-token']) return;
 
   const random = nanoid();
 
-  // const res = NextResponse.redirect(req.nextUrl.clone());
-  // res.cookie('poll-token', random, { sameSite: 'strict' });
-  // return res;
+  const res = NextResponse.redirect(req.nextUrl.clone());
+  res.cookie('poll-token', random, { sameSite: 'strict' });
+  return res;
 }
