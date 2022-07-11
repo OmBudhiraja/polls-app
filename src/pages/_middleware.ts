a287244feafd72faa63a9c0e7fd44fe5ead60328
Om Budhiraja <ombudhiraja144@gmail.com>
2022-07-11 20:13:06 +0530

    cloning url before redirecting

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -2,22 +2,12 @@ import { NextResponse } from 'next/server';
 import type { NextFetchEvent, NextRequest } from 'next/server';
 import { nanoid } from 'nanoid';
 
-// export function middleware(req: NextRequest, ev: NextFetchEvent) {
-//   if (req.cookies['poll-token']) return;
-
-//   const random = nanoid();
-
-//   const res = NextResponse.next();
-//   res.cookie('poll-token', random, { sameSite: 'strict' });
-//   return res;
-// }
-
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   if (req.cookies['poll-token']) return;
 
   const random = nanoid();
 
-  const res = NextResponse.redirect(req.nextUrl);
+  const res = NextResponse.redirect(req.nextUrl.clone());
   res.cookie('poll-token', random, { sameSite: 'strict' });
   return res;
 }
